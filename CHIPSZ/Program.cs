04e028a054d9f2e80cc95c59fbe686c5accd2e78
stevethomas13 <101266186+stevethomas13@users.noreply.github.com>
2023-02-12 18:39:53 +0000

    added working 5 targets that change position after a 5 second interval

--- CHIPSZ/Program.cs
+++ CHIPSZ/Program.cs
@@ -1,10 +1,15 @@
 using StereoKit;
 using System;
+using System.Collections;
+using System.Timers;
 
 namespace CHIPSZ
 {
     internal class Program
     {
+        private static Timer targetTimer;
+        private static ArrayList cubes = new ArrayList();
+        private static ArrayList cubesPoses = new ArrayList();
         static void Main(string[] args)
         {
             // Initialize StereoKit
@@ -16,12 +21,31 @@ namespace CHIPSZ
             if (!SK.Initialize(settings))
                 Environment.Exit(1);
 
+            targetTimer = new System.Timers.Timer();
+            targetTimer.Interval = 5000;
+            targetTimer.Elapsed += changeCubePoses;
+            targetTimer.AutoReset = true;
+            targetTimer.Enabled = true;
 
-            // Create assets used by the app
-            Pose cubePose = new Pose(0, 0, -0.5f, Quat.Identity);
-            Model cube = Model.FromMesh(
-                Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
-                Default.MaterialUI);
+
+            Random randomNumberGenerator = new Random();
+
+            for( int i = 0; i < 5; i++)
+            {
+                float targetPosX = (float)( randomNumberGenerator.Next( -30, 30 ) / 10.0 );
+                float targetPosY = (float)( randomNumberGenerator.Next( -10, 10 ) / 10.0 );
+
+                //targetPosX = 
+
+                // Create assets used by the app
+                Pose cubePose = new Pose(targetPosX, targetPosY, -2f, Quat.Identity);
+                Model cube = Model.FromMesh(
+                    Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
+                    Default.MaterialUI);
+                cubes.Add(cube);
+                cubesPoses.Add(cubePose);
+            }
+            
 
             Matrix floorTransform = Matrix.TS(0, -1.5f, 0, new Vec3(30, 0.1f, 30));
             Material floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
@@ -34,10 +58,31 @@ namespace CHIPSZ
                 if (SK.System.displayType == Display.Opaque)
                     Default.MeshCube.Draw(floorMaterial, floorTransform);
 
-                UI.Handle("Cube", ref cubePose, cube.Bounds);
-                cube.Draw(cubePose.ToMatrix());
+                //UI.Handle("Cube", ref cubePose, cube.Bounds);
+                for( int i = 0; i < 5; i++)
+                {
+                    Model cube = (Model)cubes[i];
+                    Pose cubePose = (Pose)cubesPoses[i];
+                    cube.Draw(cubePose.ToMatrix());
+                }
             })) ;
             SK.Shutdown();
+        }
+
+        private static void changeCubePoses(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Random randomNumberGenerator = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                float targetPosX = (float)(randomNumberGenerator.Next(-30, 30) / 10.0);
+                float targetPosY = (float)(randomNumberGenerator.Next(-10, 10) / 10.0);
+
+                //targetPosX = 
+
+                // Create assets used by the app
+                Pose cubePose = new Pose(targetPosX, targetPosY, -2f, Quat.Identity);
+                cubesPoses[i] = cubePose;
+            }
         }
     }
 }
