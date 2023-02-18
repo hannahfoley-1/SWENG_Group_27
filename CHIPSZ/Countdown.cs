fb14ebd0ba5f8a1facd2f5ed60f9a72de175b436
Ivan Puig <ivan@bpuig.com>
2023-02-18 08:52:54 +0000

    Add countdown functionality

---
+++ CHIPSZ/Countdown.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+
+namespace CHIPSZ
+{
+    internal class Countdown
+    {
+        private int remainingTime;
+        private Timer timer;
+        private bool isRunning;
+
+        public Countdown(int remainingTime)
+        {
+            this.remainingTime = remainingTime;
+            timer = new Timer(1000);
+            timer.Elapsed += TimerElapsedHandler;
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            timer.Start();
+            this.isRunning = true;
+        }
+
+        private void TimerElapsedHandler (Object sender, EventArgs e)
+        {
+            remainingTime--;
+            Console.WriteLine($"{remainingTime} seconds left");
+
+            if (remainingTime == 0) timer.Stop();
+        }
+
+        public bool IsRunning()
+        {
+            return this.isRunning;
+        }
+
+    }
+}
