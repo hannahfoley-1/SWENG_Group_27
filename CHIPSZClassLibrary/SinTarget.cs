4cbdb501b39a62818e50c67a2930fbca72d4be3a
stevethomas13 <101266186+stevethomas13@users.noreply.github.com>
2023-03-16 06:19:18 +0000

    added file for sin targets

---
+++ CHIPSZClassLibrary/SinTarget.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StereoKit;
+
+namespace CHIPSZClassLibrary
+{
+    class SinTarget : Target
+    {
+        private static readonly int POINTS = 10;
+        int counter = 1;
+        public SinTarget() : base( POINTS )
+        {
+        }
+
+        public override void Move( float speed )
+        {
+            Pose coords = GetPose();
+            coords.position.y += 0.5f * (float)Math.Sin(counter * Math.PI / 180) ;
+            coords.position.z += speed;
+            if (coords.position.z >= 1) this.SetHidden(true);
+            else
+            {
+                this.SetPose(coords);
+            }
+        }
+    }
+}
