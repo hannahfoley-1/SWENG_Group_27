1422678d6c85417ea1ab097655e18cef6f363fc6
Ivan Puig <ivan@bpuig.com>
2023-03-17 05:12:07 +0000

    Create finish screen
    When the coundown goes to zero, players will be sent to
    this screen where they will be asked to choose whether
    they want to keep playing, see their highscores/statistics
    or to exit the game.

---
+++ CHIPSZClassLibrary/FinishScreen.cs
@@ -0,0 +1,28 @@
+﻿using StereoKit;
+
+namespace CHIPSZClassLibrary
+{
+  public class FinishScreen
+  {
+
+    private Pose finishPose;
+
+    public FinishScreen() {
+      finishPose = new Pose(new Vec3(0, .2f, -.3f), Quat.LookDir(1, 0, 1));
+    }
+
+    public void Update() {
+      GameOverScreen();
+    }
+
+    // Initial Screen where the user will be prompt after the timer runs out
+    public void GameOverScreen() {
+      UI.WindowBegin("\tGame Over", ref finishPose, new Vec2(20, 10) * U.cm, UIWin.Normal);
+      UI.WindowEnd();
+    }
+
+  }
+}
+
+
+
