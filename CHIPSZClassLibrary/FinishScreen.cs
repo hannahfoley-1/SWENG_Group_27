de27972dc0867c4f1663ab796de0ca0bd4d2cb77
Ivan Puig <ivan@bpuig.com>
2023-03-17 22:34:53 +0000

    Implement button menu

--- CHIPSZClassLibrary/FinishScreen.cs
+++ CHIPSZClassLibrary/FinishScreen.cs
@@ -1,28 +1,39 @@
 ﻿using StereoKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CHIPSZClassLibrary
+namespace CHIPSZ
 {
-  public class FinishScreen
-  {
+    internal class FinishScreen
+    {
+        private Pose finishPose;
 
-    private Pose finishPose;
+        public FinishScreen()
+        {
+            finishPose = new Pose(new Vec3(0, .2f, -.3f), Quat.LookDir(0, 0, 1));
+        }
 
-    public FinishScreen() {
-      finishPose = new Pose(new Vec3(0, .2f, -.3f), Quat.LookDir(1, 0, 1));
+        public void Update()
+        {
+            GameOverScreen();
+        }
+
+        // Initial Screen where the user will be prompt after the timer runs out
+        public void GameOverScreen()
+        {
+            UI.WindowBegin("", ref finishPose, new Vec2(20, 10) * U.cm, UIWin.Body);
+            UI.PopTextStyle();
+            UI.Text("Game Over", TextAlign.Center);
+            UI.HSeparator();
+            UI.Button("Try again");
+            UI.Button("See game statistics");
+            UI.Button("Leave the game");
+            UI.WindowEnd();
+        }
+
     }
-
-    public void Update() {
-      GameOverScreen();
-    }
-
-    // Initial Screen where the user will be prompt after the timer runs out
-    public void GameOverScreen() {
-      UI.WindowBegin("\tGame Over", ref finishPose, new Vec2(20, 10) * U.cm, UIWin.Normal);
-      UI.WindowEnd();
-    }
-
-  }
 }
-
-
 
