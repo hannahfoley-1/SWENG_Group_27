8dc5db08009133f1495350f38fe42706a28c7a8b
pjhrolfe <rolfep@tcd.ie>
2023-03-21 16:50:07 +0000

    Fixed branch

--- CHIPSZ/Program.cs
+++ CHIPSZ/Program.cs
@@ -13,13 +13,13 @@ namespace CHIPSZ
         private static Countdown countdown;
         private static ProjectileGenerator ballGenerator;
         private static TargetGenerator targetGenerator;
+        private static TargetGenerator demoTargets;
         private static Floor floor;
-		    private static StartingScreen screen;
+		private static StartingScreen screen;
         private static FinishScreen finishScreen;
         private static AudioManager audioManager;
         private static GameTimer spawnBallTimer;
         private static GameState gameState;
-        private static StartingScreen screen;
         private static PauseMenu pauseMenu;
         private static bool paused;
 
@@ -46,7 +46,7 @@ namespace CHIPSZ
                 Environment.Exit(1);
 
             audioManager = new AudioManager();
-            countdown = new Countdown(5); // sets the game duration to 90 seconds
+            countdown = new Countdown(90); // sets the game duration to 90 seconds
             countdown.SetRunning(false);
             floor = new Floor();
             screen = new StartingScreen();
@@ -65,47 +65,14 @@ namespace CHIPSZ
                 new HandMenuItem("Stance 1", null, () => stance = true))));
 
             Initialise();
-        /*
-        static void Main(string[] args)
-        {
-            AudioManager audioManager = new AudioManager();
 
-            // Initialize StereoKit
-            SKSettings settings = new SKSettings
-            {
-                appName = "StereoKitProject1",
-                assetsFolder = "Assets",
-            };
-            if (!SK.Initialize(settings))
-                Environment.Exit(1);
-
-            countdown = new Countdown(90); // sets the game duration to 90 seconds
-            countdown.SetRunning(false);
-            floor = new Floor();
-            screen = new StartingScreen();
-
-            // Create pause menu:
             pauseMenu = new PauseMenu();
             paused = false;
-
-            ballGenerator = new ProjectileGenerator();
-            targetGenerator = new TargetGenerator();
-            TargetGenerator demoTargets = new TargetGenerator();
-
-
-            GameTimer spawnBallTimer = new GameTimer(0.5);
-
-            // Core application loop
-            //while (countdown.IsRunning() && SK.Step(() => // when the time runs out the app closes
-            //booleans to switch between game and demo states
-            bool closeForGame = screen.GetIfStartGame();
-            bool closeForDemo = screen.GetIfStartDemo();
-            */
 
             bool tempFlipWaterFireSpawn = false;
 
             Hand hand = Input.Hand(Handed.Right);
-            Vec3 handPreviousFrame;
+            Vec3 handPreviousFrame = Vec3.Zero;
             Vec3 scoreTextPos = new Vec3(-1.0f, 0.9f, -2.0f);
             while (countdown.GetDuration() > 0.0 && SK.Step(() => // when the time runs out the app closes
             {
@@ -124,6 +91,10 @@ namespace CHIPSZ
                 
                 // Debug stance toggle
                 if (Input.Key(Key.M).IsJustActive())
+                {
+                    stance = !stance;
+                }
+
                 // Draw pause menu, check for input
                 pauseMenu.Draw();
                 paused = pauseMenu.GetPaused();
@@ -140,49 +111,57 @@ namespace CHIPSZ
                 hand = Input.Hand(Handed.Right);
                 spawnBallTimer.Update();
                 screen.Draw(gameState);
-                
+
                 //Pose solidCurrentPose;
                 //GAME STATE:
                 if (gameState == GameState.GAME)
-                {                                   
+                {
                     ballGenerator.ResetPlayerScore();
-                    
+
                     countdown.SetRunning(true);
                     if (countdown.GetDuration() == 0.0)
                         ballGenerator.ResetPlayerScore();
-                /*
-                if (!paused)
-                {
-                    handPreviousFrame = hand.palm.position;
-                    hand = Input.Hand(Handed.Right);
-                    spawnBallTimer.Update();
-                    screen.Draw();
-                    closeForGame = screen.GetIfStartGame();
-                    closeForDemo = screen.GetIfStartDemo();
-                */
 
+                    hand.Solid = false;
+                    if (SK.System.displayType == Display.Opaque)
+                        Default.MeshCube.Draw(floor.GetMaterial(), floor.GetTransform());
 
-                    //Pose solidCurrentPose;
-                    //GAME STATE:
-                    if (closeForGame == false)
+                    if (Input.Key(Key.MouseRight).IsJustActive() || hand.IsJustGripped)
                     {
-                        //ballGenerator.ResetPlayerScore();
-                        countdown.SetRunning(true);
-
-                        hand.Solid = false;
-                        if (SK.System.displayType == Display.Opaque)
-                            Default.MeshCube.Draw(floor.GetMaterial(), floor.GetTransform());
-
-                        if (Input.Key(Key.MouseRight).IsJustActive() || hand.IsJustGripped)
+                        if (spawnBallTimer.elasped)
                         {
-                            if (spawnBallTimer.elasped)
+                            ballGenerator.SpawnProjectile(hand, Element.EARTH);
+                            AudioManager.Instance.Play("StoneCast-Modified", hand.palm.position, 1f);
+                            spawnBallTimer.Reset();
+                        }
+                    }
+
+                    else if (Input.Key(Key.F).IsJustActive() || (GetVelocity(hand.palm.position, handPreviousFrame).z < -3f && hand.gripActivation == 0))
+                    {
+
+                        if (spawnBallTimer.elasped)
+                        {
+                            if (!stance)
                             {
-                                ballGenerator.SpawnProjectile(hand, Element.EARTH);
-                                audioManager.Play("spawnBall", hand.palm.position, 1f);
+                                ballGenerator.SpawnProjectile(hand, Element.FIRE);
+                                AudioManager.Instance.Play("spawnBall", hand.palm.position, 1f);
                                 spawnBallTimer.Reset();
+                                AudioManager.Instance.Play("FireCast-Modified", hand.palm.position, 1f);
+                                spawnBallTimer.Reset();
+                                tempFlipWaterFireSpawn = false;
                             }
-                            
-                       }
+
+                            else
+                            {
+                                ballGenerator.SpawnProjectile(hand, Element.WATER);
+                                AudioManager.Instance.Play("spawnBall", hand.palm.position, 1f);
+                                spawnBallTimer.Reset();
+                                AudioManager.Instance.Play("WaterCast-Modified", hand.palm.position, 1f);
+                                spawnBallTimer.Reset();
+                                tempFlipWaterFireSpawn = true;
+                            }
+
+                        }
                     }
 
                     ballGenerator.Update(hand);
@@ -196,105 +175,63 @@ namespace CHIPSZ
                     if (SK.System.displayType == Display.Opaque)
                         Default.MeshCube.Draw(floor.GetMaterial(), floor.GetTransform());
 
-                    if(screen.PlayDemo1() == true)
+                    if (screen.PlayDemo1() == true)
                     {
-                        if(screen.PlayDemo2() == true)
+                        if (screen.PlayDemo2() == true)
                         {
                             screen.PlayDemo3();
                             demoTargets.Draw();
                             demoTargets.CheckHit(ballGenerator.GetAllProjectiles(), ballGenerator, hand);
                         }
-
-                        else if (Input.Key(Key.F).IsJustActive() || (GetVelocity(hand.palm.position, handPreviousFrame).z < -3f && hand.gripActivation == 0))
-                        {
-
-                            if (spawnBallTimer.elasped)
-                            {
-                                if (tempFlipWaterFireSpawn)
-                                {
-                                    ballGenerator.SpawnProjectile(hand, Element.FIRE);
-                                    audioManager.Play("spawnBall", hand.palm.position, 1f);
-                                    spawnBallTimer.Reset();
-                                    tempFlipWaterFireSpawn = false;
-                                }
-
-                                else
-                                {
-                                    ballGenerator.SpawnProjectile(hand, Element.WATER);
-                                    audioManager.Play("spawnBall", hand.palm.position, 1f);
-                                    spawnBallTimer.Reset();
-                                    tempFlipWaterFireSpawn = true;
-                                }
-
-                            }
-                        }
-
-                        //Text.Add("Score :" + targetGenerator.targetsHit, Matrix.TRS(scoreTextPos, Quat.FromAngles(0, 180.0f, 0), 10.0f));
-                        ballGenerator.Update(hand);
-                        ballGenerator.Draw(false);
-                        targetGenerator.Draw();
-                        targetGenerator.CheckHit(ballGenerator.GetAllProjectiles(), ballGenerator, hand);
                     }
-                    //DEMO STATE:
-                    else if (closeForDemo == false)
+
+                    if (Input.Key(Key.MouseRight).IsJustActive() || hand.IsJustGripped)
                     {
-                        if (SK.System.displayType == Display.Opaque)
-                            Default.MeshCube.Draw(floor.GetMaterial(), floor.GetTransform());
-
-                        if (screen.PlayDemo1() == true)
+                        if (spawnBallTimer.elasped)
                         {
-                            if (screen.PlayDemo2() == true)
-                            {
-                                screen.PlayDemo3();
-                                demoTargets.Draw();
-                                demoTargets.CheckHit(ballGenerator.GetAllProjectiles(), ballGenerator, hand);
-                            }
-                        }
-
-                        if (Input.Key(Key.MouseRight).IsJustActive() || hand.IsJustGripped)
-                        {
-                            if (spawnBallTimer.elasped)
-                            {
-                                ballGenerator.SpawnProjectile(hand, Element.EARTH);
-                                audioManager.Play("spawnBall", hand.palm.position, 1f);
-                                spawnBallTimer.Reset();
-                            }
-                        }
-                    }                   
-                        else if (Input.Key(Key.F).IsJustActive() || GetVelocity(hand.palm.position, handPreviousFrame).z < -3f && hand.gripActivation == 0)
-                        {
-                            if (spawnBallTimer.elasped)
-                            {
-                                if (tempFlipWaterFireSpawn)
-                                {
-                                    ballGenerator.SpawnProjectile(hand, Element.FIRE);
-                                    audioManager.Play("spawnBall", hand.palm.position, 1f);
-                                    spawnBallTimer.Reset();
-                                    tempFlipWaterFireSpawn = false;
-                                }
-
-                                else
-                                {
-                                    ballGenerator.SpawnProjectile(hand, Element.WATER);
-                                    audioManager.Play("spawnBall", hand.palm.position, 1f);
-                                    spawnBallTimer.Reset();
-                                    tempFlipWaterFireSpawn = true;
-                                }
-                            }
-                        }
-
-                        ballGenerator.Update(hand);
-                        ballGenerator.Draw(true);
-
-                        if (screen.GetIfEndDemo())
-                        {
-                            screen.SetIfStartDemo(true);
-                            screen.SetIfStartGame(false);
+                            ballGenerator.SpawnProjectile(hand, Element.EARTH);
+                            AudioManager.Instance.Play("StoneCast-Modified", hand.palm.position, 1f);
+                            spawnBallTimer.Reset();
                         }
                     }
+                    else if (Input.Key(Key.F).IsJustActive() || GetVelocity(hand.palm.position, handPreviousFrame).z < -3f && hand.gripActivation == 0)
+                    {
+                        if (spawnBallTimer.elasped)
+                        {
+                            if (tempFlipWaterFireSpawn)
+                            {
+                                ballGenerator.SpawnProjectile(hand, Element.FIRE);
+                                AudioManager.Instance.Play("spawnBall", hand.palm.position, 1f);
+                                spawnBallTimer.Reset();
+                                AudioManager.Instance.Play("FireCast-Modified", hand.palm.position, 1f);
+                                spawnBallTimer.Reset();
+                                tempFlipWaterFireSpawn = false;
+                            }
+
+                            else
+                            {
+                                ballGenerator.SpawnProjectile(hand, Element.WATER);
+                                AudioManager.Instance.Play("spawnBall", hand.palm.position, 1f);
+                                spawnBallTimer.Reset();
+                                AudioManager.Instance.Play("WaterCast-Modified", hand.palm.position, 1f);
+                                spawnBallTimer.Reset();
+                                tempFlipWaterFireSpawn = true;
+                            }
+                        }
+                    }
+                    ballGenerator.Update(hand);
+                    ballGenerator.Draw(true);
                 }
+                handPreviousFrame = hand.palm.position;
                 countdown.Update();
+                if (countdown.GetDuration() <= 0)
+                {
+                    finishScreen.Update();
+                    if (finishScreen.OptionSelected() && finishScreen.IsReset()) Initialise();
+
+                }
             })) ;
+            SK.RemoveStepper(handMenu);
             SK.Shutdown();
         }
 
