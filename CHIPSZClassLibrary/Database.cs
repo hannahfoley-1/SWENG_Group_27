ea57ec2d3d65ecbf80e8bc2261bd8ec709b4ab1b
Ivan Puig <ivan@bpuig.com>
2023-03-21 18:15:37 +0000

    Add "database"

---
+++ CHIPSZClassLibrary/Database.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CHIPSZClassLibrary
+{
+    internal class Database
+    {
+        private Dictionary<string, string> records;
+        private string filename;
+
+        public Database(string filename)
+        {
+            this.filename = filename;
+            records = new Dictionary<string, string>();
+
+            if(System.IO.File.Exists(filename))
+            {
+                ReadAll();
+            }
+            else
+            {
+                System.IO.File.Create(filename);
+            }
+        }
+
+        public void ReadAll()
+        {
+
+            foreach (string line in System.IO.File.ReadAllLines(filename))
+            {
+                if (!string.IsNullOrEmpty(line) && line.Contains('='))
+                {
+                    int idx = line.IndexOf('=');    
+                    string key = line.Substring(0, idx).Trim();
+                    string value = line.Substring(idx + 1).Trim();
+                    records.Add(key, value);
+                }
+            }
+        }
+
+        public void Add(string field, string value)
+        {
+            System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(filename);
+            streamWriter.WriteLine($"{field}={value}");
+            streamWriter.Close();
+        }
+
+    }
+}
+
